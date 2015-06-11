3f4ac5f0917bf8b90287c5854a5927b37c3bb4a3
Omer Akram <om26er@gmail.com>
2015-06-12 02:24:44 +0500

    More readable code, beginning of a cleanup

--- app/src/main/java/com/byteshaft/ghostrecorder/AppGlobals.java
+++ app/src/main/java/com/byteshaft/ghostrecorder/AppGlobals.java
@@ -1,21 +1,19 @@
 package com.byteshaft.ghostrecorder;
 
+import android.util.Log;
 
 public class AppGlobals {
-
-    static final String LOG_TAG = "SPY";
-
-    private static boolean isRecording;
-
-    static boolean isRecording() {
-        return isRecording;
-    }
-
-    static void setIsRecording(boolean state) {
-        isRecording = state;
-    }
 
     static final int SERVER_DIED = 100;
     static final int STOPPED_AFTER_TIME = 101;
     static final int STOPPED_WITH_DIRECT_CALL = 102;
+    private static final String LOG_TAG = "SPY";
+
+    static String getLogTag(Class aClass) {
+        return LOG_TAG + "/" + aClass.getSimpleName();
+    }
+
+    static void logInformation(String tag, String message) {
+        Log.i(tag, message);
+    }
 }
