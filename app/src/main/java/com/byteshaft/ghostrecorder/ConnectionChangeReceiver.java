bd59039e53f97727d17830f7957aa56aec002236
Omer Akram <om26er@gmail.com>
2015-06-12 03:15:23 +0500

    Cleanup connection listener

--- app/src/main/java/com/byteshaft/ghostrecorder/ConnectionChangeReceiver.java
+++ app/src/main/java/com/byteshaft/ghostrecorder/ConnectionChangeReceiver.java
@@ -9,19 +9,21 @@ import android.util.Log;
 
 public class ConnectionChangeReceiver extends BroadcastReceiver {
 
+    private String LOG_TAG = AppGlobals.getLogTag(getClass());
+
     @Override
-    public void onReceive( Context context, Intent intent )
-    {
+    public void onReceive(Context context, Intent intent) {
         ConnectivityManager connectivityManager = (ConnectivityManager)
-                context.getSystemService( Context.CONNECTIVITY_SERVICE );
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
-        NetworkInfo mobNetInfo = connectivityManager.getNetworkInfo (
-                ConnectivityManager.TYPE_MOBILE );
-        if ( activeNetInfo != null ){
-            Log.i("Network", "Active network type " + activeNetInfo);
+        NetworkInfo mobNetInfo = connectivityManager.getNetworkInfo(
+                ConnectivityManager.TYPE_MOBILE);
+        if (activeNetInfo != null) {
+            AppGlobals.logInformation(LOG_TAG, "Active network type " + activeNetInfo);
         }
-        if( mobNetInfo != null ){
-            Log.i("Network", "Mobile network type"+ mobNetInfo);
+
+        if( mobNetInfo != null ) {
+            AppGlobals.logInformation(LOG_TAG, "Mobile network type"+ mobNetInfo);
         }
     }
 }
