efc13c77c84e0021a9589a13c2952faa24be81ea
Imran <linuxholic@hotmail.com>
2015-06-12 16:48:24 +0500

    Implement delay time for scheduled recording

--- app/src/main/java/com/byteshaft/ghostrecorder/CallStateListener.java
+++ app/src/main/java/com/byteshaft/ghostrecorder/CallStateListener.java
@@ -1,17 +1,22 @@
 package com.byteshaft.ghostrecorder;
 
+import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
 public class CallStateListener extends PhoneStateListener {
 
+    RecorderHelpers mRecordHelpers;
+    Context context;
+
     @Override
     public void onCallStateChanged(int state, String incomingNumber) {
+
         super.onCallStateChanged(state, incomingNumber);
         switch (state) {
             case TelephonyManager.CALL_STATE_RINGING:
                 if (CustomMediaRecorder.isRecording()) {
-                    RecorderHelpers.stopRecording();
+                    mRecordHelpers.stopRecording();
                 }
                 break;
         }
